11b075ecc618d91b0f4d5a194da916a3a84405ad
Emma <emma_a284@hotmail.com>
2017-09-07 21:02:40 -0300

    comit inicial

--- app/src/main/java/grupoabpr/dam/isi/frsf/Lab01/MainActivity.java
+++ app/src/main/java/grupoabpr/dam/isi/frsf/Lab01/MainActivity.java
@@ -1,6 +1,7 @@
 package grupoabpr.dam.isi.frsf.Lab01;
 
 import android.content.Context;
+import android.content.pm.ActivityInfo;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
